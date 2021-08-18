373f5458a4def4a331c1358e2b0ac6d2a8bd75f4
Rico Perez <ricoperez1@gmail.com>
2021-08-17 20:02:01 -0500

    adding routes

---
+++ Develop/models/index.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: new Date(new Date().setDate(new Date().getDate()))
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },
+    }, ],
+});
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = { Workout };
---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const db = require("./models");
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+// whatever my db is named as endpoint
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
